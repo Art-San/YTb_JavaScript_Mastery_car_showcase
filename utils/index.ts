117864abf1646ccd592f1b4c5118a123f236074b
Aleksander <asava@sibmail.com>
2023-07-21 00:28:04 +0700

    .env добавил

--- utils/index.ts
+++ utils/index.ts
@@ -1,33 +1,33 @@
 import { ICarsProps } from '@/types'
 export async function fetchCars() {
-    const headers = {
-        'X-RapidAPI-Key': '9de32da2eemshf3e549597ac9ae2p15d9b0jsn1e6791b653dd',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-    }
+  const headers = {
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY_ASAVA || '',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
 
-    const response = await fetch(
-        // 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
-        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
+  const response = await fetch(
+    // 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
 
-        { headers: headers }
-    )
-    const result = await response.json()
-    return result
+    { headers: headers }
+  )
+  const result = await response.json()
+  return result
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 50 // Базовая стоимость аренды в сутки в долларах
-    const mileageFactor = 0.1 // Дополнительная ставка за милю пробега
-    const ageFactor = 0.05 // Доплата за год возраста автомобиля
+  const basePricePerDay = 50 // Базовая стоимость аренды в сутки в долларах
+  const mileageFactor = 0.1 // Дополнительная ставка за милю пробега
+  const ageFactor = 0.05 // Доплата за год возраста автомобиля
 
-    // Рассчитать дополнительную ставку на основе пробега и возраста
-    const mileageRate = city_mpg * mileageFactor
-    const ageRate = (new Date().getFullYear() - year) * ageFactor
+  // Рассчитать дополнительную ставку на основе пробега и возраста
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
 
-    // Рассчитать общую арендную ставку в день
-    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+  // Рассчитать общую арендную ставку в день
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
-    return rentalRatePerDay.toFixed(0)
+  return rentalRatePerDay.toFixed(0)
 }
 
 // export const generateCarImageUrl = (car: ICarsProps, angle?: string) => {
@@ -50,16 +50,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 // }
 
 export const generateCarImageUrl = (car: ICarsProps, angle?: string) => {
-    const url = new URL('https://cdn.imagin.studio/car-image-api')
+  const url = new URL('https://cdn.imagin.studio/car-image-api')
 
-    const { make, year, model } = car
+  const { make, year, model } = car
 
-    url.searchParams.append('customer', 'hrjavascript-mastery')
-    url.searchParams.append('make', make)
-    url.searchParams.append('modelFamily', model.split(' ')[0])
-    url.searchParams.append('zoomType', 'fullscreen')
-    url.searchParams.append('modelYear', `${year}`)
-    url.searchParams.append('angle', `${angle}`)
+  url.searchParams.append('customer', 'hrjavascript-mastery')
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
 
-    return `${url}`
+  return `${url}`
 }
