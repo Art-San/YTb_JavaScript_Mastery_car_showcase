e51f34feb3387ec743de7a1a9644b083ad9cd1e0
Aleksander <asava@sibmail.com>
2023-07-20 23:24:33 +0700

    Search, Server-side rendering

--- types/index.ts
+++ types/index.ts
@@ -1,31 +1,43 @@
 import { MouseEventHandler } from 'react'
 
 export interface ICustomButtonProps {
-    title: string
-    containerStyles?: string
-    handleClick?: MouseEventHandler<HTMLButtonElement>
-    btnType?: 'button' | 'submit'
-    textStyles?: string
-    rightIcon?: string
-    isDisabled?: boolean
+  title: string
+  containerStyles?: string
+  handleClick?: MouseEventHandler<HTMLButtonElement>
+  btnType?: 'button' | 'submit'
+  textStyles?: string
+  rightIcon?: string
+  isDisabled?: boolean
 }
 
 export interface ISearchManufacturerProps {
-    manufacturer: string
-    setManuFacturer: (manufacturer: string) => void
+  manufacturer: string
+  setManuFacturer: (manufacturer: string) => void
 }
 
 export interface ICarsProps {
-    city_mpg: number
-    class: string
-    combination_mpg: number
-    cylinders: number
-    displacement: number
-    drive: string
-    fuel_type: string
-    highway_mpg: number
-    make: string
-    model: string
-    transmission: string
-    year: number
+  city_mpg: number
+  class: string
+  combination_mpg: number
+  cylinders: number
+  displacement: number
+  drive: string
+  fuel_type: string
+  highway_mpg: number
+  make: string
+  model: string
+  transmission: string
+  year: number
+}
+
+export interface IFilterProps {
+  manufacturer?: string
+  year?: number
+  model?: string
+  limit?: number
+  fuel?: string
+}
+
+export interface IHomeProps {
+  searchParams: IFilterProps
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,65 +1,52 @@
-import { ICarsProps } from '@/types'
-export async function fetchCars() {
-    const headers = {
-        'X-RapidAPI-Key': '9de32da2eemshf3e549597ac9ae2p15d9b0jsn1e6791b653dd',
-        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+import { ICarsProps, IFilterProps } from '@/types'
+
+export async function fetchCars(filters: IFilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters
+
+  // Set the required headers for the API request
+  const headers = {
+    // 'X-RapidAPI-Key': '9de32da2eemshf3e549597ac9ae2p15d9b0jsn1e6791b653dd',
+    'X-RapidAPI-Key': 'f1e2a7e825msh0f3b08053979f5dp160532jsn94e8643efaf5', // asava.sibmail@gmail.com
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers
     }
-
-    const response = await fetch(
-        // 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
-        'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',
-
-        { headers: headers }
-    )
-    const result = await response.json()
-    return result
+  )
+  const result = await response.json()
+  return result
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 50 // Базовая стоимость аренды в сутки в долларах
-    const mileageFactor = 0.1 // Дополнительная ставка за милю пробега
-    const ageFactor = 0.05 // Доплата за год возраста автомобиля
+  const basePricePerDay = 50 // Базовая стоимость аренды в сутки в долларах
+  const mileageFactor = 0.1 // Дополнительная ставка за милю пробега
+  const ageFactor = 0.05 // Доплата за год возраста автомобиля
 
-    // Рассчитать дополнительную ставку на основе пробега и возраста
-    const mileageRate = city_mpg * mileageFactor
-    const ageRate = (new Date().getFullYear() - year) * ageFactor
+  // Рассчитать дополнительную ставку на основе пробега и возраста
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
 
-    // Рассчитать общую арендную ставку в день
-    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+  // Рассчитать общую арендную ставку в день
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
-    return rentalRatePerDay.toFixed(0)
+  return rentalRatePerDay.toFixed(0)
 }
 
-// export const generateCarImageUrl = (car: ICarsProps, angle?: string) => {
-//     const url = new URL('https://cdn.imagin.studio/getimage')
-//     const { make, model, year } = car
-
-//     // url.searchParams.append(
-//     //     'customer',
-//     //     process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ''
-//     // )
-//     url.searchParams.append('customer', 'hrjavascript-mastery')
-//     url.searchParams.append('make', make)
-//     url.searchParams.append('modelFamily', model.split(' ')[0])
-//     url.searchParams.append('zoomType', 'fullscreen')
-//     url.searchParams.append('modelYear', `${year}`)
-//     // url.searchParams.append('zoomLevel', zoomLevel);
-//     url.searchParams.append('angle', `${angle}`)
-
-//     return `${url}`
-// }
-
 export const generateCarImageUrl = (car: ICarsProps, angle?: string) => {
-    const url = new URL('https://cdn.imagin.studio/car-image-api')
+  const url = new URL('https://cdn.imagin.studio/car-image-api')
 
-    const { make, year, model } = car
+  const { make, year, model } = car
 
-    url.searchParams.append('customer', 'hrjavascript-mastery')
-    url.searchParams.append('make', make)
-    url.searchParams.append('modelFamily', model.split(' ')[0])
-    url.searchParams.append('zoomType', 'fullscreen')
-    url.searchParams.append('modelYear', `${year}`)
-    url.searchParams.append('angle', `${angle}`)
+  url.searchParams.append('customer', 'hrjavascript-mastery')
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
 
-    return `${url}`
+  return `${url}`
 }
