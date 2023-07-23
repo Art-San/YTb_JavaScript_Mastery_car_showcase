8d7b35fdf3756bdf153aca1377250e1e4e42a5df
Aleksander <asava@sibmail.com>
2023-07-23 13:21:00 +0700

    фавикон мучаю

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,22 +3,25 @@ import type { Metadata } from 'next'
 import { Footer, Navbar } from '@/components'
 
 export const metadata: Metadata = {
-    title: 'Car Hub',
-    description: 'Discover the best cars in the world.'
+  title: 'Car Hub',
+  description: 'Discover the best cars in the world.'
+  //   icons: {
+  //     icon: 'https://drive.google.com/file/d/1CNac2MMYGfnBGpxGYbQ657lmTkWEXLjv/view?usp=sharing'
+  //   }
 }
 
 export default function RootLayout({
-    children
+  children
 }: {
-    children: React.ReactNode
+  children: React.ReactNode
 }) {
-    return (
-        <html lang="en">
-            <body className="relative">
-                <Navbar />
-                {children}
-                <Footer />
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
 }
